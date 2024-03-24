8f0b6ca3c8359a75618284630f8b23d2be30abf2
Rodolpho Reis <rodolphoreis@live.com>
2024-03-24 18:53:35 +0000

    feat: create post router

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,37 @@
 import express from "express";
 import cors from "cors";
 
-import { PrismaClient } from "@prisma/client";
+import prismaCliente from "../prisma/prismaClient";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(cors());
+
+app.get("/api/user", async (_, response) => {
+  const read = await prismaCliente.user.findMany();
+  return response.json({ message: read });
+});
+
+app.post("/api/user", async (request, response) => {
+  const { name, surname, age, job, haveChildren } = request.body;
+
+  if (age < 18) {
+    return "User cannot be created.";
+  }
+
+  const create = await prismaCliente.user.create({
+    data: {
+      name,
+      surname,
+      job,
+      haveChildren,
+    },
+  });
+
+  return response.status(201).json({ message: create, erro: false });
+});
 
 app.listen(PORT, () => {
   console.log("Server ok.");
