bf81cffd66abc306bafcdd9b9efcc61b7d71622f
Rodolpho Reis <rodolphoreis@live.com>
2024-04-07 18:54:18 +0100

    refactor: user service

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 
 import prismaCliente from "../prisma/prismaClient";
+import userService from "./services/user-service";
 
 const app = express();
 const PORT = 3000;
@@ -10,74 +11,42 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/api/user", async (_, response) => {
-  const read = await prismaCliente.user.findMany();
-  return response.json({ message: read });
+  const result = await userService.read();
+  return response.json(result);
 });
 
 app.post("/api/user", async (request, response) => {
   const { name, surname, age, job, haveChildren } = request.body;
-
-  if (age < 18) {
-    return response
-      .status(400)
-      .json({
-        message:
-          "User cannot be created, because we only accept users with age above 18",
-      });
-  }
-  try {
-    const create = await prismaCliente.user.create({
-      data: {
-        name,
-        surname,
-        job,
-        haveChildren,
-        age,
-      },
-    });
-
-    return response.status(201).json({ message: create, erro: false });
-  } catch (error) {
-    return response.status(500).json({ message: error, erro: true });
-  }
+  const result = await userService.create({
+    name,
+    surname,
+    age,
+    job,
+    haveChildren,
+  });
+  return response.json(result);
 });
 
 app.put("/api/user/:id", async (request, response) => {
   const { id } = request.params;
   const { name, surname, age, job, haveChildren } = request.body;
-  try {
-    const findUser = await prismaCliente.user.findUnique({
-      where: { id },
-    });
-    if (!findUser) {
-      return response.status(404).json("User not found!");
-    }
-
-    const upDate = await prismaCliente.user.update({
-      where: { id },
-      data: {
-        name,
-        surname,
-        job,
-        haveChildren,
-      },
-    });
-    return response.status(200).json({ message: upDate, erro: false });
-  } catch (error) {
-    return response.status(500).json({ message: error, erro: true });
-  }
+  const result = await userService.update(
+    {
+      name,
+      surname,
+      age,
+      job,
+      haveChildren,
+    },
+    id
+  );
+  return response.json(result);
 });
 
 app.delete("/api/user/:id", async (request, response) => {
   const { id } = request.params;
-  try {
-    const remove = await prismaCliente.user.delete({
-      where: { id },
-    });
-    return response.status(200).json({ message: "Foi deletado!", erro: false });
-  } catch (error) {
-    return response.status(500).json({ message: error, erro: true });
-  }
+  const result = await userService.remove(id);
+  return response.json(result);
 });
 
 app.listen(PORT, () => {
