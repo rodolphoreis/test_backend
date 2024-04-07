b905e302a3fd85db881661b18b3416dd92fed700
Rodolpho Reis <rodolphoreis@live.com>
2024-04-07 19:29:31 +0100

    refactor: index

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import express from "express";
 import cors from "cors";
 
-import prismaCliente from "../prisma/prismaClient";
-import userService from "./services/user-service";
+import userRoutes from "./routes/routes";
 
 const app = express();
 const PORT = 3000;
@@ -10,44 +9,7 @@ const PORT = 3000;
 app.use(express.json());
 app.use(cors());
 
-app.get("/api/user", async (_, response) => {
-  const result = await userService.read();
-  return response.json(result);
-});
-
-app.post("/api/user", async (request, response) => {
-  const { name, surname, age, job, haveChildren } = request.body;
-  const result = await userService.create({
-    name,
-    surname,
-    age,
-    job,
-    haveChildren,
-  });
-  return response.json(result);
-});
-
-app.put("/api/user/:id", async (request, response) => {
-  const { id } = request.params;
-  const { name, surname, age, job, haveChildren } = request.body;
-  const result = await userService.update(
-    {
-      name,
-      surname,
-      age,
-      job,
-      haveChildren,
-    },
-    id
-  );
-  return response.json(result);
-});
-
-app.delete("/api/user/:id", async (request, response) => {
-  const { id } = request.params;
-  const result = await userService.remove(id);
-  return response.json(result);
-});
+app.use("/api/user", userRoutes);
 
 app.listen(PORT, () => {
   console.log("Server ok.");
